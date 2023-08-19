b9a52d68d23f0d2f8ac70e69c2d16d9bddfa3e22
ddonydev <ddonydev@gmail.com>
2023-08-19 22:02:40 +0900

    feat: MongoDB Repository 설정

--- src/main/java/com/zzdd/smallspending/SmallSpendingApplication.java
+++ src/main/java/com/zzdd/smallspending/SmallSpendingApplication.java
@@ -2,8 +2,10 @@ package com.zzdd.smallspending;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = com.zzdd.smallspending.chat.MongoDBRepository.class)
 public class SmallSpendingApplication {
 
 	public static void main(String[] args) {
